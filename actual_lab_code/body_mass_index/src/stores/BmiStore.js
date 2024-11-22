ce14d7299a549e04d1195f86aef0741432236a87
Clara <10088152+claraj@users.noreply.github.com>
2024-11-22 10:00:56 -0600

    feedback from zoom

--- actual_lab_code/body_mass_index/src/stores/BmiStore.js
+++ actual_lab_code/body_mass_index/src/stores/BmiStore.js
@@ -6,24 +6,41 @@ export const useBmiStore = defineStore('bmi', () =>{
     //variables to be used
     const userWeight = ref('')
     const userHeight = ref('')
-    const errors = ref([])
-    const bMI = ref('')
+    // const errors = ref([])
+    // const bMI = ref('')
 
-    function determineUserBMI() {
-        console.log(userHeight, userWeight)
-        errors.value= []
-        if (userHeight.value.length == 0){
-          errors.value.push('You must enter a height in meters.')
+    const errors = computed( () => {   // errors the user will see 
+
+      const error_list = [] 
+
+      if (userHeight.value.length == 0){
+          error_list.push('You must enter a height in meters.')
+        }
+        if (userHeight.value <= 0){
+          error_list.push('You must enter a positive height in meters.')
         }
         if(userWeight.value.length == 0){
-          errors.value.push('You must enter a weight in Kilograms')
+          error_list.push('You must enter a weight in Kilograms')
         }
-        if (errors.value.length > 0){
-          alert(errors.value.join('\n'))
-        }else{
-          bMI.value = Math.round(userWeight.value / (userHeight.value * userHeight.value))
+        return error_list
+    })
+
+    // This will provide the BMI if it can be calculated.
+    // 
+    const bMI = computed ( () => {
+        console.log(userHeight, userWeight)
+        if (errors.value.length > 0) {  // there are errors 
+          // ??? todo ??? for example 
+          // return undefined,
+          // return null
+          // return -1 
+          // something that means this can't be calculated 
+      
+        } else {
+          const bmi = Math.round(userWeight.value / (userHeight.value * userHeight.value))
+          return bmi
         }}
+    )
 
-
-    return userWeight, userHeight, errors, bMI, determineUserBMI
+    return {userWeight, userHeight, errors, bMI}
 })
